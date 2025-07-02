b8ce6c272a412d15f6c8ea3250ceba0b478f34c8
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2025-07-01 23:18:48 -0400

    In CollisionGroup, changed .collisions member to a method with a backing member, _collisions.

--- Source/Geometry/CollisionGroup.js
+++ Source/Geometry/CollisionGroup.js
@@ -1,23 +1,35 @@
 "use strict";
 class CollisionGroup {
     constructor(collisions) {
-        this.collisions = collisions;
+        this._collisions = collisions;
     }
     static create() {
         return new CollisionGroup([]);
     }
     clear() {
-        this.collisions.length = 0;
+        this._collisions.length = 0;
         return this;
     }
     collisionAdd(collisionToAdd) {
-        this.collisions.push(collisionToAdd);
+        var i = 0;
+        for (i = 0; i < this._collisions.length; i++) {
+            var collisionInPlace = this._collisions[i];
+            var shouldBreak = (collisionToAdd.distanceToCollision
+                <= collisionInPlace.distanceToCollision);
+            if (shouldBreak) {
+                break;
+            }
+        }
+        this._collisions.splice(i, 0, collisionToAdd);
         return this;
     }
     collisionClosest() {
-        return Collision.closestOf(this.collisions);
+        return this._collisions[0]; // Collision.closestOf(this.collisions);
+    }
+    collisions() {
+        return this._collisions;
     }
     hasCollisions() {
-        return (this.collisions.length > 0);
+        return (this._collisions.length > 0);
     }
 }
--- Source/Geometry/CollisionGroup.ts
+++ Source/Geometry/CollisionGroup.ts
@@ -1,11 +1,11 @@
 
 class CollisionGroup
 {
-	collisions: Collision[];
+	_collisions: Collision[];
 
 	constructor(collisions: Collision[])
 	{
-		this.collisions = collisions;
+		this._collisions = collisions;
 	}
 
 	static create(): CollisionGroup
@@ -15,23 +15,44 @@ class CollisionGroup
 
 	clear(): CollisionGroup
 	{
-		this.collisions.length = 0;
+		this._collisions.length = 0;
 		return this;
 	}
 
 	collisionAdd(collisionToAdd: Collision): CollisionGroup
 	{
-		this.collisions.push(collisionToAdd);
+		var i = 0;
+		for (i = 0; i < this._collisions.length; i++)
+		{
+			var collisionInPlace = this._collisions[i];
+			var shouldBreak =
+			(
+				collisionToAdd.distanceToCollision
+				<= collisionInPlace.distanceToCollision
+			);
+			if (shouldBreak)
+			{
+				break;
+			}
+		}
+
+		this._collisions.splice(i, 0, collisionToAdd);
+
 		return this;
 	}
 
 	collisionClosest(): Collision
 	{
-		return Collision.closestOf(this.collisions);
+		return this._collisions[0]; // Collision.closestOf(this.collisions);
+	}
+
+	collisions(): Collision[]
+	{
+		return this._collisions;
 	}
 
 	hasCollisions(): boolean
 	{
-		return (this.collisions.length > 0);
+		return (this._collisions.length > 0);
 	}
 }
